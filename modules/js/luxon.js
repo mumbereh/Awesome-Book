219d2b593d11d3e6cc7bf7c54707958abaa27551
mumbereh <siirasonh@gmail.com>
2023-07-19 09:56:59 +0300

    Rectified all the required changes. i.e using arrow function in all required modules & displaying datetime on the page

--- modules/js/luxon.js
+++ modules/js/luxon.js
@@ -1,9 +1,9 @@
 const displayDateTime = () => {
-    const DateTime = luxon.DateTime.local().toISO();
-  
-    const displayTimeParagraph = document.querySelector('.display-time');
-  
-    displayTimeParagraph.textContent = `Current DateTime: ${DateTime}`;
-  };
-  
-  displayDateTime();
+  const DateTime = luxon.DateTime.local().toISO();
+
+  const displayTimeParagraph = document.querySelector('.display-time');
+
+  displayTimeParagraph.textContent = `DateTime: ${DateTime}`;
+};
+
+displayDateTime();
--- modules/js/toggleSection.js
+++
@@ -1,6 +0,0 @@
-const toggleSection = (sectionId) => {
-  const section = document.getElementById(sectionId);
-  section.style.display = section.style.display === 'none' ? 'block' : 'none';
-};
-
-export default toggleSection;
