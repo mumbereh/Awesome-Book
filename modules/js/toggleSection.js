be20fdc78e75c7e704d310b6f56f57333cdc87df
mumbereh <siirasonh@gmail.com>
2023-07-19 09:26:57 +0300

    Rectified all the required changes. i.e using arrow function in all required modules & displaying datetime on the page

--- modules/js/toggleSection.js
+++ modules/js/toggleSection.js
@@ -1,4 +1,6 @@
-export default function toggleSection(sectionId) {
+const toggleSection = (sectionId) => {
   const section = document.getElementById(sectionId);
   section.style.display = section.style.display === 'none' ? 'block' : 'none';
-}
+};
+
+export default toggleSection;
